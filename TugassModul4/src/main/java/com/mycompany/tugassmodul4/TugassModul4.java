f267b42a98d79e153aa6a52ded0897abd80cc591
Deviana57 <140058849+Deviana57@users.noreply.github.com>
2024-09-04 09:20:22 +0700

    Add files via upload

---
+++ TugassModul4/src/main/java/com/mycompany/tugassmodul4/TugassModul4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugassmodul4;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class TugassModul4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
