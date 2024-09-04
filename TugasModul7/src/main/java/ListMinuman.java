4d3e7273ea7fc1a6db21f2e6b81404d906c55c8a
Deviana57 <140058849+Deviana57@users.noreply.github.com>
2024-09-04 15:18:03 +0700

    Add files via upload

---
+++ TugasModul7/src/main/java/ListMinuman.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JScrollPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class ListMinuman {
+
+    static void setModel(JScrollPane jListMinuman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
